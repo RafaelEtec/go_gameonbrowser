6a1feebe7ebb17b5f8de29d1f93a445c19b2614b
Rafael Goulart <rafaelxms@hotmail.com>
2025-01-30 22:01:53 -0300

    initial resources

--- main.go
+++ main.go
@@ -4,27 +4,15 @@ import (
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/rafaeletec/go_gameonbrowser/tictactoe"
 )
 
-type Game struct{}
-
-func (g *Game) Update() error {
-	return nil
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
-}
-
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
-}
-
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	game := tictactoe.NewGame()
+
+	ebiten.SetWindowSize(tictactoe.ScreenWidth, tictactoe.ScreenHeight)
+	ebiten.SetWindowTitle("TicTacToe by Rafael Goulart")
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
